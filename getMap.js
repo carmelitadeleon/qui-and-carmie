7f169f3d3376d9e6278c630737f2db4a7b01043f
carmelitadeleon <4376059+carmelitadeleon@users.noreply.github.com>
2025-02-17 22:43:10 -0800

    Fix Details page

---
+++ getMap.js
@@ -0,0 +1,33 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// Initialize and add the map
+// let map;
+//
+// async function initMap() {
+//   // The location of Uluru
+//
+//   const position = { lat: 47.920521, lng: -122.222527 };
+//   // Request needed libraries.
+//   //@ts-ignore
+//   const { Map } = await google.maps.importLibrary("maps");
+//   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+//
+//   // The map, centered at St. Mary Magdalen Parish
+//   map = new Map(document.getElementById("map"), {
+//     zoom: 15,
+//     center: position,
+//     mapId: "DEMO_MAP_ID",
+//   });
+//
+//   // The marker, positioned at St. Mary Magdalen
+//   const marker = new AdvancedMarkerElement({
+//     map: map,
+//     position: position,
+//     title: "St. Mary Magdalen Parish",
+//   });
+// }
+//
+// initMap();
